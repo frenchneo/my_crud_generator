22420accc5ba1823b7c1c77094952bc50b5cc828
frenchneo <58567825+frenchneo@users.noreply.github.com>
2023-08-30 00:05:56 +0200

    feat: :sparkles: Crud Generator : Get & Create

--- crud_generator.php
+++ crud_generator.php
@@ -17,13 +17,14 @@ function createEntity()
     }
 
     echo "Les champs suivants ont été ajoutés à l'entité product : \n";
+    print_r($fields);
     foreach ($fields as $field) {
         echo "entityName : " . $field['entityName'] . ", fieldName : " . $field['fieldName'] . ", type : " . $field['type'] . "\n";
     }
     $sqlFileName = $entityName . ".sql";
-    createFile($sqlFileName);
-    writeInfile($sqlFileName, createSqlSchema($fields));
-    writeInfile($entityName . ".php", createEntityClass($fields));
+    writeInfile('src/' . $sqlFileName, createSqlSchema($fields));
+    writeInfile('src/create' . ucfirst($entityName) . ".php", generateCreateFile($fields));
+    writeInfile('src/get' . ucfirst($entityName) . ".php", generateGetFile($fields));
 }
 
 function createFile($name)
@@ -49,159 +50,102 @@ function createSqlSchema($fields)
     return $sql;
 }
 
-function createEntityClass($fields)
+function generateCreateFile($fields)
 {
-    $className = $fields[0]['entityName'];
-    $class = "<?php\n";
-    $class = "class " . $className . " {\n";
-    $class .= "    private \$id;\n";
-    foreach ($fields as $field) {
-        $class .= "    private \$" . $field['fieldName'] . ";\n";
-    }
-    $class .= "\n";
-    $class .= "    public function __construct(";
-    $count = 0;
-    foreach ($fields as $field) {
-        $count > 0 ? $class .= ", " : $class .= "";
-        $class .= "$" . $field['fieldName'];
-        $count++;
-    }
-    $class .= ") {\n";
-    foreach ($fields as $field) {
-        $class .= "        \$this->" . $field['fieldName'] . " = $" . $field['fieldName'] . ";\n";
-    }
-    $class .= "    }\n";
-    $class .= "\n";
-    $class .= "    public function save() {\n";
-    $class .= "        global \$conn;\n";
-    $class .= "\n";
-    $class .= "        \$sql = \"INSERT INTO " . $className . " (";
-    $count = 0;
-    foreach ($fields as $field) {
-        $count > 0 ? $class .= ", " : $class .= "";
-        $class .= $field['fieldName'];
-        $count++;
-    }
-    $class .= ") VALUES (";
+    $content = "<?php\n";
+    $content .= '$data = json_decode(file_get_contents("php://input"), true);' . "\n";
+    $content .= "var_dump(\$data);\n\n";
 
-    $count = 0;
-    foreach ($fields as $field) {
-        $count > 0 ? $class .= ", " : $class .= "";
-        $class .= "'\$this->" . $field['fieldName'] . "'";
-        $count++;
+    $content .= '$servername = "localhost";' . "\n";
+    $content .= '$username = "root";' . "\n";
+    $content .= '$password = "Password$13";' . "\n";
+    $content .= '$database = "crud_generator";' . "\n\n";
+    $content .= '$conn = new mysqli($servername, $username, $password, $database);' . "\n\n";
+    $content .= 'if ($conn->connect_error) {' . "\n";
+    $content .= '    die("Erreur de connexion : " . $conn->connect_error);' . "\n";
+    $content .= '}' . "\n\n";
+
+    $sql = 'INSERT INTO ' . $fields[0]['entityName'] . ' (';
+    $fieldNames = array_column($fields, 'fieldName');
+    $sql .= implode(', ', $fieldNames);
+    $sql .= ") VALUES ('\" . ";
+
+    foreach ($fieldNames as $index => $fieldName) {
+        print($index);
+        if ($index !== 0) {
+            $sql .= '. "\', \'" . $data[\'' . "$fieldName" . '\'] ';
+        } else {
+            $sql .= "\$data['$fieldName'] ";
+        }
     }
-    $class .= ")\";\n";
-    $class .= "\n";
-    $class .= "        if (\$conn->query(\$sql) === FALSE) {\n";
-    $class .= "            echo \"Erreur lors de l'enregistrement du produit : \" . \$conn->error;\n";
-    $class .= "        }\n";
-    $class .= "    }\n";
-    $class .= "\n";
-    $class .= "    public static function index() {\n";
-    $class .= "        global \$conn;\n";
-    $class .= "\n";
-    $class .= "        \$sql = \"SELECT * FROM " . $className . "\";\n";
-    $class .= "        \$result = \$conn->query(\$sql);\n";
+    $sql .= '. "\')";' . "\n";
+    $content .= '$sql = ' . '"' . $sql;
 
-    $class .= "\n";
-    $class .= "        \$" . $className . "s = array();\n";
-    $class .= "        if (\$result->num_rows > 0) {\n";
-    $class .= "            while (\$row = \$result->fetch_assoc()) {\n";
-    $class .= "                \$" . $className . " = new " . $className . "(";
-    $count = 0;
-    foreach ($fields as $field) {
-        $count > 0 ? $class .= ", " : $class .= "";
-        $class .= "\$row['" . $field['fieldName'] . "']";
-        $count++;
-    }
-    $class .= ");\n";
-    $class .= "                \$" . $className . "->id = \$row['id'];\n";
-    $class .= "                \$" . $className . "s[] = \$" . $className . ";\n";
-    $class .= "            }\n";
-    $class .= "        }\n";
-    $class .= "\n";
-    $class .= "        return \$" . $className . "s;\n";
-    $class .= "    }\n";
-    $class .= "\n";
-    $class .= "    public static function show(\$id) {\n";
-    $class .= "        global \$conn;\n";
-    $class .= "\n";
-    $class .= "        \$sql = \"SELECT * FROM " . $className . " WHERE id = \$id\";\n";
-    $class .= "        \$result = \$conn->query(\$sql);\n";
-    $class .= "\n";
-    $class .= "        if (\$result->num_rows == 1) {\n";
-    $class .= "            \$row = \$result->fetch_assoc();\n";
-    $class .= "            \$" . $className . " = new " . $className . "(";
-    $count = 0;
+    $content .= 'if ($conn->query($sql) === TRUE) {' . "\n";
+    $content .= '    echo "Nouvel enregistrement créé avec succès";' . "\n";
+    $content .= '} else {' . "\n";
+    $content .= '    echo "Erreur : " . $sql . "<br>" . $conn->error;' . "\n";
+    $content .= '}' . "\n";
 
-    foreach ($fields as $field) {
-        $count > 0 ? $class .= ", " : $class .= "";
-        $class .= "\$row['" . $field['fieldName'] . "']";
-        $count++;
-    }
-    $class .= ");\n";
-    $class .= "            \$" . $className . "->id = \$row['id'];\n";
-    $class .= "            return \$" . $className . ";\n";
-    $class .= "        } else {\n";
-    $class .= "            return null;\n";
-    $class .= "        }\n";
-    $class .= "    }\n";
-    $class .= "\n";
+    $content .= '$conn->close();' . "\n";
 
-    $class .= "    public function load(\$id) {\n";
-    $class .= "        global \$conn;\n";
-    $class .= "\n";
-    $class .= "        \$sql = \"SELECT * FROM " . $className . " WHERE id = \$id\";\n";
-    $class .= "        \$result = \$conn->query(\$sql);\n";
-    $class .= "\n";
-    $class .= "        if (\$result->num_rows == 1) {\n";
-    $class .= "            \$row = \$result->fetch_assoc();\n";
-    $class .= "            \$this->id = \$row['id'];\n";
-    foreach ($fields as $field) {
-        $class .= "            \$this->" . $field['fieldName'] . " = \$row['" . $field['fieldName'] . "'];\n";
-    }
-    $class .= "        }\n";
-    $class .= "    }\n";
-    $class .= "\n";
-    $class .= "    public function update() {\n";
-    $class .= "        global \$conn;\n";
-    $class .= "\n";
-    $class .= "        \$sql = \"UPDATE " . $className . " SET ";
-    $count = 0;
+    $content .= '?>';
 
-    foreach ($fields as $field) {
-        $count > 0 ? $class .= ", " : $class .= "";
-        $class .= $field['fieldName'] . " = '\$this->" . $field['fieldName'] . "'";
-        $count++;
-    }
-    $class .= " WHERE id = \$this->id\";\n";
-
-    $class .= "\n";
-    $class .= "        if (\$conn->query(\$sql) === FALSE) {\n";
-    $class .= "            echo \"Erreur lors de la mise à jour du produit : \" . \$conn->error;\n";
-    $class .= "        }\n";
-    $class .= "    }\n";
-    $class .= "\n";
-
-    $class .= "    public function delete() {\n";
-    $class .= "        global \$conn;\n";
-    $class .= "\n";
-    $class .= "        \$sql = \"DELETE FROM " . $className . " WHERE id = \$this->id\";\n";
-    $class .= "\n";
-    $class .= "        if (\$conn->query(\$sql) === FALSE) {\n";
-    $class .= "            echo \"Erreur lors de la suppression du produit : \" . \$conn->error;\n";
-    $class .= "        }\n";
-    $class .= "    }\n";
-    $class .= "}\n";
-    $class .= "?>";
-
-    return $class;
+    return $content;
 }
+
+function generateGetFile($fields)
+{
+    $content = "<?php\n";
+    $content .= '$servername = "localhost";' . "\n";
+    $content .= '$username = "root";' . "\n";
+    $content .= '$password = "Password$13";' . "\n";
+    $content .= '$database = "crud_generator";' . "\n\n";
+
+    $content .= '$conn = new mysqli($servername, $username, $password, $database);' . "\n\n";
+    $content .= 'if ($conn->connect_error) {' . "\n";
+    $content .= '    die("Erreur de connexion : " . $conn->connect_error);' . "\n";
+    $content .= '}' . "\n\n";
+    // $content .= 'echo "Connexion réussie à la base de données MySQL";' . "\n\n";
+
+    $content .= '$sql = "SELECT * FROM ' . $fields[0]['entityName'] . '";' . "\n";
+    $content .= '$result = $conn->query($sql);' . "\n\n";
+
+    $content .= '$' . $fields[0]['entityName'] . 's = array();' . "\n";
+    $content .= 'if ($result->num_rows > 0) {' . "\n";
+    $content .= '    while ($row = $result->fetch_assoc()) {' . "\n";
+    $content .= '        $' . $fields[0]['entityName'] . 's[] = $row;' . "\n";
+    $content .= '    }' . "\n";
+    $content .= '}' . "\n\n";
+
+    $content .= 'echo json_encode($' . $fields[0]['entityName'] . 's);' . "\n\n";
+
+    $content .= '$conn->close();' . "\n\n";
+
+    $content .= '?>';
+
+    return $content;
+}
+
 function writeInfile($file, $content)
 {
-    $file = fopen($file, "w");
-    fwrite($file, $content);
+    $filePath = dirname($file);
+
+    if (!file_exists($filePath)) {
+        mkdir($filePath, 0777, true);
+    }
+
+    $fileHandle = fopen($file, "w");
+
+    if ($fileHandle) {
+        fwrite($fileHandle, $content);
+        fclose($fileHandle);
+        return true;
+    } else {
+        return false;
+    }
 }
+
 
 createEntity();
 ?>
--- users.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `users`
-(
-  `id` int PRIMARY KEY,
-  `jo` varchar(255),
-  `jack` int,
-  `mleh` varchar(255));
